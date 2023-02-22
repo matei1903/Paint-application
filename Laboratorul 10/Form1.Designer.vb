63097e1165e4a59292637b1d63b5a78f69ffd4d2
Matei-Alexandru Ciucă <86956363+matei1903@users.noreply.github.com>
2023-02-23 01:12:20 +0200

    Add files via upload

---
+++ Laboratorul 10/Form1.Designer.vb
@@ -0,0 +1,225 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DrawToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SettingsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ToolStripButton2 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton3 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton4 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton5 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton6 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton7 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton8 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton9 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.DrawToolStripMenuItem, Me.SettingsToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(697, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'DrawToolStripMenuItem
+        '
+        Me.DrawToolStripMenuItem.Name = "DrawToolStripMenuItem"
+        Me.DrawToolStripMenuItem.Size = New System.Drawing.Size(46, 20)
+        Me.DrawToolStripMenuItem.Text = "Draw"
+        '
+        'SettingsToolStripMenuItem
+        '
+        Me.SettingsToolStripMenuItem.Name = "SettingsToolStripMenuItem"
+        Me.SettingsToolStripMenuItem.Size = New System.Drawing.Size(61, 20)
+        Me.SettingsToolStripMenuItem.Text = "Settings"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 97)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(697, 333)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'ToolStripButton2
+        '
+        Me.ToolStripButton2.AutoSize = False
+        Me.ToolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton2.Image = Global.Laboratorul_10.My.Resources.Resources.Open
+        Me.ToolStripButton2.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None
+        Me.ToolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton2.Name = "ToolStripButton2"
+        Me.ToolStripButton2.Size = New System.Drawing.Size(73, 70)
+        Me.ToolStripButton2.Text = "ToolStripButton2"
+        '
+        'ToolStripButton3
+        '
+        Me.ToolStripButton3.AutoSize = False
+        Me.ToolStripButton3.Image = Global.Laboratorul_10.My.Resources.Resources.DrawColor
+        Me.ToolStripButton3.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None
+        Me.ToolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton3.Name = "ToolStripButton3"
+        Me.ToolStripButton3.Size = New System.Drawing.Size(74, 70)
+        Me.ToolStripButton3.Text = "Draw color"
+        Me.ToolStripButton3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
+        'ToolStripButton4
+        '
+        Me.ToolStripButton4.AutoSize = False
+        Me.ToolStripButton4.Image = Global.Laboratorul_10.My.Resources.Resources.BackColor
+        Me.ToolStripButton4.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None
+        Me.ToolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton4.Name = "ToolStripButton4"
+        Me.ToolStripButton4.Size = New System.Drawing.Size(74, 70)
+        Me.ToolStripButton4.Text = "Back color"
+        Me.ToolStripButton4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
+        'ToolStripButton5
+        '
+        Me.ToolStripButton5.AutoSize = False
+        Me.ToolStripButton5.Image = Global.Laboratorul_10.My.Resources.Resources.Line
+        Me.ToolStripButton5.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None
+        Me.ToolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton5.Name = "ToolStripButton5"
+        Me.ToolStripButton5.Size = New System.Drawing.Size(74, 70)
+        Me.ToolStripButton5.Text = "Line"
+        Me.ToolStripButton5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
+        'ToolStripButton6
+        '
+        Me.ToolStripButton6.AutoSize = False
+        Me.ToolStripButton6.Image = Global.Laboratorul_10.My.Resources.Resources.Rectangle
+        Me.ToolStripButton6.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None
+        Me.ToolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton6.Name = "ToolStripButton6"
+        Me.ToolStripButton6.Size = New System.Drawing.Size(74, 70)
+        Me.ToolStripButton6.Text = "Rectangle"
+        Me.ToolStripButton6.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
+        'ToolStripButton7
+        '
+        Me.ToolStripButton7.AutoSize = False
+        Me.ToolStripButton7.Image = Global.Laboratorul_10.My.Resources.Resources.Elipse
+        Me.ToolStripButton7.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None
+        Me.ToolStripButton7.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton7.Name = "ToolStripButton7"
+        Me.ToolStripButton7.Size = New System.Drawing.Size(74, 70)
+        Me.ToolStripButton7.Text = "Elipse"
+        Me.ToolStripButton7.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
+        'ToolStripButton8
+        '
+        Me.ToolStripButton8.AutoSize = False
+        Me.ToolStripButton8.Image = Global.Laboratorul_10.My.Resources.Resources.Freehand
+        Me.ToolStripButton8.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None
+        Me.ToolStripButton8.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton8.Name = "ToolStripButton8"
+        Me.ToolStripButton8.Size = New System.Drawing.Size(74, 70)
+        Me.ToolStripButton8.Text = "Freehand"
+        Me.ToolStripButton8.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
+        'ToolStripButton9
+        '
+        Me.ToolStripButton9.Image = Global.Laboratorul_10.My.Resources.Resources.Clear
+        Me.ToolStripButton9.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None
+        Me.ToolStripButton9.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton9.Name = "ToolStripButton9"
+        Me.ToolStripButton9.Size = New System.Drawing.Size(74, 67)
+        Me.ToolStripButton9.Text = "Clear"
+        Me.ToolStripButton9.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.AutoSize = False
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton2, Me.ToolStripButton1, Me.ToolStripButton3, Me.ToolStripButton4, Me.ToolStripButton5, Me.ToolStripButton6, Me.ToolStripButton7, Me.ToolStripButton8, Me.ToolStripButton9})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 24)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(697, 70)
+        Me.ToolStrip1.Stretch = True
+        Me.ToolStrip1.TabIndex = 1
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.AutoSize = False
+        Me.ToolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton1.Image = Global.Laboratorul_10.My.Resources.Resources.Save
+        Me.ToolStripButton1.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(77, 70)
+        Me.ToolStripButton1.Text = "ToolStripButton1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(697, 430)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DrawToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SettingsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ToolStripButton2 As ToolStripButton
+    Friend WithEvents ToolStripButton3 As ToolStripButton
+    Friend WithEvents ToolStripButton4 As ToolStripButton
+    Friend WithEvents ToolStripButton5 As ToolStripButton
+    Friend WithEvents ToolStripButton6 As ToolStripButton
+    Friend WithEvents ToolStripButton7 As ToolStripButton
+    Friend WithEvents ToolStripButton8 As ToolStripButton
+    Friend WithEvents ToolStripButton9 As ToolStripButton
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripButton1 As ToolStripButton
+End Class
---
+++ Laboratorul 10/Form1.vb
@@ -0,0 +1,171 @@
+﻿Public Class Form1
+
+    Dim myBitmap As Bitmap
+    Dim myGraphics As Graphics
+    Dim myPen As Pen
+    Dim x, y As Integer
+    Dim x1, x2, y1, y2 As Integer
+
+    Private Sub DrawLine()
+        myGraphics.DrawLine(myPen, x1, y1, x2, y2)
+        PictureBox1.Image = myBitmap
+    End Sub
+
+    Private Sub DrawRectangle()
+        myGraphics.DrawRectangle(myPen, x1, y1, 100, 50)
+        PictureBox1.Image = myBitmap
+    End Sub
+
+    Private Sub DrawElipse()
+        myGraphics.DrawEllipse(myPen, x1, y1, 100, 50)
+        PictureBox1.Image = myBitmap
+    End Sub
+
+    Private Sub DrawPixel()
+        myGraphics.DrawLine(myPen, x1, y1, x1, y1)
+        PictureBox1.Image = myBitmap
+    End Sub
+
+    Private Sub ToolStripButton2_Click(sender As Object, e As EventArgs) Handles ToolStripButton2.Click
+
+        Dim open As New OpenFileDialog
+        open.Filter = "JPG Files (*.jpg)|*.jpg|Bitmaps (*.bmp)|*.bmp|Gif (*.gif)|*.gif|AllFiles|*.*"
+        open.FilterIndex = 1
+        If open.ShowDialog = DialogResult.OK Then
+            myBitmap = Image.FromFile(open.FileName)
+            myGraphics = Graphics.FromImage(myBitmap)
+            PictureBox1.Image = myBitmap
+        End If
+
+    End Sub
+
+    Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles ToolStripButton1.Click
+
+        Dim save As New SaveFileDialog
+        save.InitialDirectory = "c:\"
+        save.Filter = "JPG Files (*.jpg)|*.jpg|Bitmaps (*.bmp)|*.bmp|Gif (*.gif)|*.gif"
+        save.FilterIndex = 1
+        If save.ShowDialog = DialogResult.OK Then
+            myBitmap.Save(save.FileName)
+        End If
+
+
+    End Sub
+
+    Private Sub ToolStripButton3_Click(sender As Object, e As EventArgs) Handles ToolStripButton3.Click
+
+        Dim colordialog1 As New ColorDialog
+
+        colordialog1.ShowDialog()
+        myPen.Color = ColorDialog1.Color
+
+    End Sub
+
+    Private Sub ToolStripButton4_Click(sender As Object, e As EventArgs) Handles ToolStripButton4.Click
+
+        Dim colordialog1 As New ColorDialog
+
+        colordialog1.ShowDialog()
+        myGraphics.Clear(ColorDialog1.Color)
+        PictureBox1.Image = myBitmap
+
+    End Sub
+
+    Private Sub ToolStripButton5_Click(sender As Object, e As EventArgs) Handles ToolStripButton5.Click
+
+        tip_desen = "linie"
+
+    End Sub
+
+    Private Sub ToolStripButton6_Click(sender As Object, e As EventArgs) Handles ToolStripButton6.Click
+
+        tip_desen = "dreptunghi"
+
+    End Sub
+
+    Private Sub ToolStripButton7_Click(sender As Object, e As EventArgs) Handles ToolStripButton7.Click
+
+        tip_desen = "elipsa"
+
+    End Sub
+
+    Private Sub ToolStripButton8_Click(sender As Object, e As EventArgs) Handles ToolStripButton8.Click
+
+        tip_desen = "creion"
+
+    End Sub
+
+    Private Sub ToolStripButton9_Click(sender As Object, e As EventArgs) Handles ToolStripButton9.Click
+
+        myGraphics.Clear(Color.White)
+        PictureBox1.Image = myBitmap
+
+    End Sub
+
+    Dim lastPoint As Point
+    Dim down = False
+    Dim tip_desen
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        myBitmap = New Bitmap(PictureBox1.Width, PictureBox1.Height)
+        myGraphics = Graphics.FromImage(myBitmap)
+        myPen = New Pen(Color.Black, 2)
+        PictureBox1.BackColor = Color.White
+
+
+
+    End Sub
+
+
+    Private Sub PictureBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove
+
+        If Not down Then Exit Sub
+        Select Case tip_desen
+            Case "creion"
+                If e.Button = Windows.Forms.MouseButtons.Left Then
+                    myGraphics.DrawLine(myPen, lastPoint, e.Location)
+                    PictureBox1.Image = myBitmap
+                    lastPoint = e.Location
+                    Me.Refresh()
+                End If
+        End Select
+
+    End Sub
+
+    Private Sub PictureBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseDown
+
+        down = True
+        x1 = e.X
+        x2 = e.X
+        y1 = e.Y
+        y2 = e.Y
+        lastPoint = e.Location
+
+    End Sub
+
+    Private Sub PictureBox1_MouseUp(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseUp
+
+        down = False
+        'Save the new point
+        x2 = e.X
+        y2 = e.Y
+        Select Case tip_desen
+            Case "linie"
+                DrawLine()
+            Case "dreptunghi"
+                DrawRectangle()
+            Case "punct"
+                DrawPixel()
+            Case "elipsa"
+                DrawElipse()
+            Case "creion"
+                If e.Button = Windows.Forms.MouseButtons.Left Then
+                    myGraphics.DrawLine(myPen, lastPoint, e.Location)
+                    PictureBox1.Image = myBitmap
+                    lastPoint = e.Location
+                    Me.Refresh()
+                End If
+        End Select
+
+    End Sub
+End Class
